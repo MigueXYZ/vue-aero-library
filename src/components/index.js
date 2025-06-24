20f16d07270689c527582aff666469c87829d597
MigueXYZ <85558979+MigueXYZ@users.noreply.github.com>
2025-06-24 23:23:49 +0100

    feat: restructure component exports in index.js for improved readability

--- src/components/index.js
+++ src/components/index.js
@@ -1,17 +1,29 @@
-export { default as DarkAeroButton } from '@/components/dark-aero/DarkAeroButton.vue'
-export { default as DarkAeroBalloon } from '@/components/dark-aero/DarkAeroBalloon.vue'
-export { default as DarkAeroCheckbox } from '@/components/dark-aero/DarkAeroCheckbox.vue'
-export { default as DarkAeroCodeBlock } from '@/components/dark-aero/DarkAeroCodeBlock.vue'
-export { default as DarkAeroSidebar } from '@/components/dark-aero/DarkAeroSidebar.vue'
-export { default as DarkAeroTable } from '@/components/dark-aero/DarkAeroTable.vue'
+// --- IMPORTS -------------------------------------------------
+import DarkAeroButton   from './dark-aero/DarkAeroButton.vue'
+import DarkAeroBalloon  from './dark-aero/DarkAeroBalloon.vue'
+import DarkAeroCheckbox from './dark-aero/DarkAeroCheckbox.vue'
+import DarkAeroCodeBlock from './dark-aero/DarkAeroCodeBlock.vue'
+import DarkAeroSidebar  from './dark-aero/DarkAeroSidebar.vue'
+import DarkAeroTable    from './dark-aero/DarkAeroTable.vue'
 
+// --- NAMED EXPORTS ------------------------------------------
+export {
+  DarkAeroButton,
+  DarkAeroBalloon,
+  DarkAeroCheckbox,
+  DarkAeroCodeBlock,
+  DarkAeroSidebar,
+  DarkAeroTable
+}
+
+// --- PLUGIN (DEFAULT) ---------------------------------------
 export default {
-    install(app){
-        app.component('DarkAeroButton', DarkAeroButton)
-        app.component('DarkAeroBalloon', DarkAeroBalloon)
-        app.component('DarkAeroCheckbox', DarkAeroCheckbox)
-        app.component('DarkAeroCodeBlock', DarkAeroCodeBlock)
-        app.component('DarkAeroSidebar', DarkAeroSidebar)
-        app.component('DarkAeroTable', DarkAeroTable)
-    }
+  install(app) {
+    app.component('DarkAeroButton',   DarkAeroButton)
+    app.component('DarkAeroBalloon',  DarkAeroBalloon)
+    app.component('DarkAeroCheckbox', DarkAeroCheckbox)
+    app.component('DarkAeroCodeBlock',DarkAeroCodeBlock)
+    app.component('DarkAeroSidebar',  DarkAeroSidebar)
+    app.component('DarkAeroTable',    DarkAeroTable)
+  }
 }
