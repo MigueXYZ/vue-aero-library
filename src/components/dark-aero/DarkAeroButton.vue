26a32a2b32ba92e7642bc54712e5763c4549ff05
MigueXYZ <85558979+MigueXYZ@users.noreply.github.com>
2025-06-27 01:22:19 +0100

    feat: update DarkAeroButton styles for improved aesthetics and interaction effects

--- src/components/dark-aero/DarkAeroButton.vue
+++ src/components/dark-aero/DarkAeroButton.vue
@@ -38,25 +38,22 @@ button {
   padding: 5px 11px;
   font-size: 15px;
   font-weight: 300;
-  /* usa a variável para o texto */
   color: var(--text-color);
-  border: 1px solid rgba(255, 255, 255, 0.15);
   border-radius: 3px;
-
-  background:
-    linear-gradient(to bottom right, rgba(255,255,255,0.06), rgba(255,255,255,0.02)),
-    linear-gradient(to top left,    rgba(255,255,255,0.08), rgba(255,255,255,0.03));
   backdrop-filter: blur(12px);
   background-blend-mode: screen;
-
-
+  border:1px solid var(--base-color);
+  background: linear-gradient(
+    0deg,
+    rgba(0, 0, 0, 0.2) 0%,         
+    rgba(112, 112, 112, 0.2) 80%,  
+    rgba(150, 150, 150, 0.2) 100%  
+  );
   box-shadow:
-    inset 0 1px 1px rgba(255,255,255,0.2),
-    0 4px 12px rgba(0,0,0,0.5),
-    0 0 10px var(--initial-glow);
+    0 4px 12px rgba(0, 0, 0, 0.2),
+    0 0 8px var(--initial-glow);
 
-
-  text-shadow: 0 1px 2px rgba(0, 0, 0, 0.4);
+  text-shadow: 0 1px 2px rgba(0, 0, 0, 0.3);
   transition:
     box-shadow 2s ease-in-out,
     background 0.3s ease,
@@ -66,18 +63,18 @@ button {
 
 button:hover:not(:disabled) {
   box-shadow:
-    inset 0 1px 1px rgba(255,255,255,0.25),
-    0 6px 14px rgba(0,0,0,0.5),
-    0 0 20px var(--glow-color);
+    inset 0 1px 1px rgba(255,255,255,0.15),
+    0 6px 10px rgba(0,0,0,0.2),
+    0 0 16px var(--glow-color);
   transform: translateY(-1px);
 }
 
 button:active:not(:disabled) {
   transform: translateY(1px);
   box-shadow:
-    inset 0 2px 4px rgba(0, 0, 0, 0.2),
-    0 2px 6px rgba(0, 0, 0, 0.3),
-    0 0 12px var(--glow-color);
+    inset 0 2px 4px rgba(0, 0, 0, 0.1),
+    0 2px 5px rgba(0, 0, 0, 0.2),
+    0 0 10px var(--glow-color);
 }
 
 button:disabled {
